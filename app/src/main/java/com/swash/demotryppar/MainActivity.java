543cf80fcdfcbe442d80d0c3adb340938866d064
tapanpatro <tapanpatro.mech@gmail.com>
2017-07-18 08:46:55 +0530

    final commit

--- app/src/main/java/com/swash/demotryppar/MainActivity.java
+++ app/src/main/java/com/swash/demotryppar/MainActivity.java
@@ -2,9 +2,8 @@ package com.swash.demotryppar;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,14 +11,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.swash.demotryppar.fragment.AboutUsFragment;
+import com.swash.demotryppar.fragment.ApplicationFormFragment;
+import com.swash.demotryppar.fragment.BlogFragment;
+import com.swash.demotryppar.fragment.CertificationFragment;
+import com.swash.demotryppar.fragment.FeedBackFragment;
+import com.swash.demotryppar.fragment.GrievancesFragment;
+import com.swash.demotryppar.fragment.KBFragment;
+import com.swash.demotryppar.fragment.LiveChatFragment;
+import com.swash.demotryppar.fragment.ManageProfiel;
+import com.swash.demotryppar.fragment.NewsEventsFragment;
+import com.swash.demotryppar.fragment.ProductDeskFragment;
+import com.swash.demotryppar.fragment.WhitepaperFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private AboutUsFragment aboutUsFragment = new AboutUsFragment();
+    private ManageProfiel manageProfiel = new ManageProfiel();
+    private ProductDeskFragment productDeskFragment = new ProductDeskFragment();
+    private BlogFragment blogFragment = new BlogFragment();
+    private WhitepaperFragment whitepaperFragment = new WhitepaperFragment();
+    private ApplicationFormFragment applicationFormFragment = new ApplicationFormFragment();
+    private CertificationFragment certificationFragment = new CertificationFragment();
+    private FeedBackFragment feedBackFragment = new FeedBackFragment();
+    private GrievancesFragment grievancesFragment = new GrievancesFragment();
+    private NewsEventsFragment newsEventsFragment = new NewsEventsFragment();
+    private LiveChatFragment liveChatFragment = new LiveChatFragment();
+    private KBFragment kbFragment = new KBFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,21 +107,36 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_aboutUs) {
             // Handle the camera action
 
             getFragmentManager().beginTransaction().replace(R.id.container_main,aboutUsFragment).commit();
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_manage_profile) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,manageProfiel).commit();
+        } else if (id == R.id.nav_productDesk) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,productDeskFragment).commit();
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_blog) {
 
-        } else if (id == R.id.nav_manage) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,blogFragment).commit();
+        } else if (id == R.id.nav_white_paper) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            getFragmentManager().beginTransaction().replace(R.id.container_main,whitepaperFragment).commit();
+        } else if (id == R.id.nav_applicationForm) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,applicationFormFragment).commit();
+        }else if (id == R.id.nav_certification) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,certificationFragment).commit();
+        } else if (id == R.id.nav_feedBack) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,feedBackFragment).commit();
+        } else if (id == R.id.nav_grivinces) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,grievancesFragment).commit();
+        } else if (id == R.id.nav_news_events) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,newsEventsFragment).commit();
+        } else if (id == R.id.nav_liveChat) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,liveChatFragment).commit();
+        } else if (id == R.id.nav_kb) {
+            getFragmentManager().beginTransaction().replace(R.id.container_main,kbFragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
